906e84ed5a2c2e32e822e09c623934b6dd289790
Vinicius Paes Berna <vini_berna2.0@hotmail.com>
2023-07-11 01:28:35 -0300

    feat: route auth/register finished

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { genSalt, hash, compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 import User from "../database/models/User";
 
 const AuthController = {
@@ -7,26 +8,37 @@ const AuthController = {
     const { name, email, password, passwordConfirmation } = req.body;
 
     if (!name) {
-      return res.status(422).json({ message: "Name is required!" });
+      return res
+        .status(422)
+        .json({ message: "O Nome é obrigatório!", fieldMessage: "name" });
     }
     if (!email) {
-      return res.status(422).json({ message: "E-mail is required!" });
+      return res
+        .status(422)
+        .json({ message: "O E-mail é obrigatório!", fieldMessage: "email" });
     }
     if (!password) {
-      return res.status(422).json({ message: "Password is required!" });
+      return res
+        .status(422)
+        .json({ message: "A senha é obrigatória!", fieldMessage: "password" });
     }
     if (password.length < 6) {
-      return res
-        .status(422)
-        .json({ message: "Password must be at least 6 characters long" });
+      return res.status(422).json({
+        message: "A senha deve conter no mínimo 6 caracteres",
+        fieldMessage: "password",
+      });
     }
     if (!passwordConfirmation) {
-      return res
-        .status(422)
-        .json({ message: "Password Confirmation is required" });
+      return res.status(422).json({
+        message: "A confirmação de senha é obrigatória",
+        fieldMessage: "passwordConfirmation",
+      });
     }
     if (password !== passwordConfirmation) {
-      return res.status(422).json({ message: "The passwords must match!" });
+      return res.status(422).json({
+        message: "A senha de confirmação não confere!",
+        fieldMessage: "passwordConfirmation",
+      });
     }
 
     const userExists = await User.findOne({
@@ -36,27 +48,36 @@ const AuthController = {
     });
     const userEmail = userExists?.dataValues.email;
     if (userEmail === email) {
-      return res.status(422).json({ message: "E-mail is already in use" });
+      return res.status(422).json({
+        message: "Esse E-mail já está em uso.",
+        fieldMessage: "email",
+      });
     }
 
     const salt = await genSalt(12);
     const passwordHash = await hash(password, salt);
 
-    const response = await User.create({
-      name,
-      email,
-      password: passwordHash,
-    });
+    try {
+      const response = await User.create({
+        name,
+        email,
+        password: passwordHash,
+      });
 
-    if (response) {
-      return res.status(202).json({
-        message: `User ${response.dataValues.name} has been registered!`,
+      if (response) {
+        return res.status(202).json({
+          message: `Usuário ${response.dataValues.name} foi registrado com sucesso!`,
+          fieldMessage: "toast",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+
+      return res.status(500).json({
+        message: "Algo deu errado! por favor tente mais tarde.",
+        fieldMessage: "toast",
       });
     }
-
-    return res
-      .status(500)
-      .json({ message: "Something went wrong! Please try again later." });
   },
 
   login: async (req: Request, res: Response) => {
@@ -83,6 +104,21 @@ const AuthController = {
 
     if (!checkPassword) {
       return res.status(422).json({ message: "Invalid password!" });
+    }
+
+    try {
+      const secret = process.env.APP_SECRET;
+      const token = sign({ id: user.id }, secret);
+
+      return res
+        .status(202)
+        .json({ message: "User authenticated with success" });
+    } catch (err) {
+      console.log(err);
+
+      return res
+        .status(500)
+        .json({ message: "Something went wrong! Please try again later." });
     }
   },
 };
--- src/http.ts
+++ src/http.ts
@@ -1,17 +1,16 @@
+import "reflect-metadata";
 import express from "express";
+import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
-import dotenv from "dotenv";
-import "reflect-metadata";
-
-dotenv.config({ path: "../.env" });
-
 import { router } from "./routes";
-
 import "./database";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(router);
